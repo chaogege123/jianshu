a4c284de4987091ec3210c4739196dbe00a27276
chaogege123 <zc1231423>
2018-04-18 11:59:58 +0800

    第二次提交

--- new/jianshu/layouts/default.vue
+++ new/jianshu/layouts/default.vue
@@ -6,47 +6,16 @@
 </template>
 
 <script>
-import MyFooter from '~/components/Footer.vue'
-
+import Vue from 'vue'
+import BoostrapVue from 'bootstrap-vue'
+Vue.use(BoostrapVue)
+// 首先引入bootstrap。css文件，加载bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+// 引入bootstrap-vue.css文件
 export default {
-  components: {
-    MyFooter
-  }
+
+
 }
 </script>
 
 <style>
-.container
-{
-  margin: 0;
-  width: 100%;
-  padding: 100px 0;
-  text-align: center;
-}
-
-.button, .button:visited
-{
-  display: inline-block;
-  color: black;
-  letter-spacing: 1px;
-  background-color: #fff;
-  border: 2px solid #000;
-  text-decoration: none;
-  text-transform: uppercase;
-  padding: 15px 45px;
-}
-
-.button:hover, .button:focus
-{
-  color: #fff;
-  background-color: #000;
-}
-
-.title
-{
-  color: #000;
-  font-weight: 300;
-  font-size: 2.5em;
-  margin: 0;
-}
-</style>
