4284b92bf1de12a6f2bcf71c5b88bbfb5e393576
chaogege123 <zc1231423>
2018-04-19 17:43:57 +0800

    首页页头

--- new/jianshu/layouts/default.vue
+++ new/jianshu/layouts/default.vue
@@ -1,22 +1,25 @@
 <template>
-  <div>
+  <div id="default-template">
     <nuxt/>
     
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-// import BoostrapVue from 'bootstrap-vue'
-// Vue.use(BoostrapVue)
-// // 首先引入bootstrap。css文件，加载bootstrap
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-// 引入bootstrap-vue.css文件
+
+
 export default {
 
 
 }
 </script>
 
-<style>
+<style scoped>
+  body{
+    padding-top:56px;
+    min-width: 786px;
+    font-size: 17px;
+
+  }
+</style>
+
--- new/jianshu/pages/index.vue
+++ new/jianshu/pages/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-  <p>hello World</p>
+  <div style="height:100%">
+  <my-header></my-header>
+  <p>我的主题内容部分</p>
   </div>
 </template>
 <script>
+import myHeader from '../components/myHeader.vue'
 export default {
+  head: {
+    title: '简书-创作你的创作',
+    meta: [
+      { charset: "utf-8" },
+      { name: "keywords", content: "简书首页" },
+      { name: "dscription", content: "简书首页" },
+      { name: "author", content: "赵超" }
+    ]
+  },
+    components:{
+            myHeader
+        },
   data() {
     return {
       name: "首页"
