4bdc2193a064af502ee65ca3ced42f6c29853688
chaogege123 <zc1231423>
2018-04-19 10:34:58 +0800

    登录页面基本已经完成

--- new/jianshu/layouts/sign.vue
+++ new/jianshu/layouts/sign.vue
@@ -20,106 +20,182 @@ export default {
 </script>
 
 <style >
-body{
-    background-color:#f1f1f1 
-}
-.sign{
-    height: 100%;
-    font-size: 14px;
-    text-align: center;
-}
-.sign .logo{
-    position: absolute;
-    left: 50px;
-    top: 50px;
-}
-.sign img{
-    width: 100px
-}
-.sign .main{
-    width: 400px;
-    margin: 50px 0 0;
-    padding: 50px 50px 30px;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 8px rgba(0, 0, 0, .1);
-    display: inline-block;
-    vertical-align: middle;
-}
-.sign .main .title{
-    margin: 0 auto 30px ;
-    color: #939393;
-    font-size: 18px;
-}
-.sign .main h4 a,.sign .main h4 b{
-    padding: 10px;
-    color:#969696;
-    font-weight: 700;
-}
-.sign .main h4 a.active{
-    font-weight: 400;
-    color:#ea6f5a;
-    border-bottom: 2px solid #ea6f5a;
-}
-.sign .main h4 a:hover{
-    font-weight: 700;
-    border-bottom: 2px solid #ea6f5a;
-    
-}
-.sign .main form{
-    color: #969696;
-    margin: 0 0 30px;
-}
-.sign .main form .input-prepend{
-    position: relative;
-    width: 100%;
-}
-.sign .main form .input-prepend input{
-    width: 100%;
-    height: 50px;
-    border: 1px solid #c8c8c8;
-    border-radius: 2px;
-    background-color: rgb(248, 248, 248);
-    padding: 4px 12px 4px 10px;
-    
-    
-}
-.sign .main form .input-prepend:nth-child(1) input{
-    border-bottom: 0;
-    border-radius: 2px 2px 0 0
-}
-.sign .main form .input-prepend:nth-child(2) input{
-    border-radius: 0 0 2px  2px
-}
-.sign .main form .input-prepend i{
-    position: absolute;
-    left: 12px;
-    top:18px;
-    font-size: 20px;
-    color: #969696
-}
-.sign .main form .remember{
-    float: left;
-    margin: 20px 0;
-}
-.sign .main form .help{
-    float: right;
-    margin: 20px 0;
-    color:#969696
-}
-.sign .main form .help:hover{
-    color:#333
-}
-.sign .main form button{
-    width: 100%;
-    margin:20px 0 0;
-    padding: 9px 18px;
-    text-align: center;
-    color: #fff;
-    background-color: #3194d0
-}
-.sign .main form button:horver{
-    color:#187cb7;
-                                       
-}
+.sign {
+        height:100%;
+        font-size:14px;
+        text-align:center;
+        background-color:#f1f1f1;
+        min-height:750px;
+    }
+    .sign:before {
+        content: "";
+        display: inline-block;
+        height: 85%;
+        vertical-align: middle;
+    }
+    .sign .logo {
+        position:absolute;
+        margin-left:50px;
+        top:56px;
+    }
+    .sign img {
+        width:100px;
+    }
+    .sign .main {
+        width:400px;
+        margin:60px auto 0;
+        padding:50px 50px 30px;
+        background:#fff;
+        border-radius: 4px;
+        box-shadow: 0 0 8px rgba(0,0,0,.1);
+        display:inline-block;
+        vertical-align: middle;
+    }
+    .sign .main .title {
+        margin:0 auto 50px;
+        color:#939393;
+        font-size:18px;
+    }
+    .sign .main .title h4 {
+        font-size:18px;
+    }
+    .sign .main h4 a{
+        font-weight:400;
+        padding:10px;
+        color:#969696;
+    }
+    .sign .main h4 b {
+        font-weight:700;
+        padding:10px;
+        color:#969696;
+    }
+    .sign .main h4 a.active {
+        font-weight:700;
+        color:#ea6f5a;
+        border-bottom:2px solid #ea6f5a;
+    }
+    .sign .main h4 a:hover {
+        border-bottom:2px solid #ea6f5a;
+    }
+    .sign .main form {
+        color:#969696;
+        margin:0 0 30px;
+    }
+    .sign .main form .input-prepend {
+        position:relative;
+        width:100%;
+    }
+    .sign .main form .input-prepend input {
+        width:100%;
+        height:50px;
+        padding:4px 12px 4px 30px;
+        border:1px solid #c8c8c8;
+        border-radius: 2px;
+        background-color:rgb(248,248,248);
+    }
+    .sign .main form .input-prepend:nth-of-type(1) input {
+        border-bottom:0;
+        border-radius:2px 2px 0 0;
+    }
+    .sign .main form .input-prepend:nth-of-type(2) input {
+        border-radius: 0 0 2px 2px;
+    }
+    .sign .main form .input-prepend i {
+        position:absolute;
+        left:10px;
+        top:15px;
+        font-size:20px;
+        color:#969696;
+    }
+    .sign .main form .remember {
+        float:left;
+        margin:15px 0;
+    }
+    .sign .main form .help {
+        float:right;
+        margin:15px 0;
+        color:#969696;
+    }
+    .sign .main form .help:hover {
+        color:#333;
+    }
+    .sign .main form button {
+        width:100%;
+        padding:9px 18px;
+        text-align:center;
+        color:#fff;
+        background:#3194D0;
+        border-radius:25px;
+        font-size:18px;
+        border:none;
+        outline: none;
+        clear:both;
+    }
+    .sign .main form button:hover {
+        background:#187cb7;
+        cursor: pointer;
+    }
+    .sign .main .more-sign p {
+        position:relative;
+        font-size:14px;
+        color:#b8b8b8;
+        margin-bottom:20px;
+    }
+    .sign .main .more-sign p:before {
+        content:'';
+        width:60px;
+        border-bottom:1px solid #b8b8b8;
+        position:absolute;
+        left:30px;
+        top:10px;
+    }
+    .sign .main .more-sign p:after {
+        content:'';
+        width:60px;
+        border-bottom:1px solid #b8b8b8;
+        position:absolute;
+        right:30px;
+        top:10px;
+    }
+    .sign .main .more-sign ul {
+        margin-bottom:10px;
+    }
+    .sign .main .more-sign ul li {
+        display:inline-block;
+        color:white;
+        margin:0 5px;
+        border-radius: 50%;
+    }
+    .sign .main .more-sign ul li a {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        display: block;
+    }
+    .sign .main .more-sign ul li a i {
+        font-size:24px;
+        text-align:center;
+        line-height:50px;
+    }
+    .sign .main .more-sign ul li a i.fa-weibo {
+        color:#e05244;
+    }
+    .sign .main .more-sign ul li a i.fa-weixin {
+        color:#00bb29;
+    }
+    .sign .main .more-sign ul li a i.fa-qq {
+        color:#498ad5;
+    }
+    @media (max-width:768px) {
+        .sign .logo {
+            display:none;
+        }
+        .sign .main {
+            box-shadow: none;
+            margin:0 auto;
+        }
+        .sign {
+            background:#fff;
+        }
+    }
 </style>
